519195a50a91c768ffe3db2289643aad462b482a
Jefferson Hovey <jefferson.hovey@gmail.com>
2025-01-29 13:11:11 -0500

    fix: unclear Reptomancer

--- src/lib/enemies/act3/elites/reptomancer.svelte.ts
+++ src/lib/enemies/act3/elites/reptomancer.svelte.ts
@@ -37,28 +37,27 @@ const reptomancer: Enemy = $derived({
   flowchart: `
     flowchart-elk TB
       subgraph Reptomancer [<img src=${base}/assets/act3/elites/reptomancer.webp class="h-10 object-contain inline" />]
-        subgraph Moves
-          A(${summon.effect}<img src=${summon.intent} class="h-10 object-contain" /><span class="text-xs">Max Consecutive: 2</span>)
-          B(${snakeStrike.effect}<img src=${snakeStrike.intent} class="h-10 object-contain" /><span class="text-xs">Max Consecutive: 1</span>)
-          C(${bigBite.effect}<img src=${bigBite.intent} class="h-10 object-contain" /><span class="text-xs">Max Consecutive: 1</span>)
-        end
-        subgraph 1-3Daggers [1-3 Daggers]
-          A1(<img src=${summon.intent} class="h-10 object-contain" /><span class="text-xs">33%</span>)
-          B1(<img src=${snakeStrike.intent} class="h-10 object-contain" /><span class="text-xs">33%</span>)
-          C1(<img src=${bigBite.intent} class="h-10 object-contain" /><span class="text-xs">33%</span>)
-        end
-        subgraph 4Daggers [4 Daggers]
-          B2(<img src=${snakeStrike.intent} class="h-10 object-contain" /><span class="text-xs">67%</span>)
+        subgraph 0-3Daggers [0-3 Daggers]
+          A2(<img src=${summon.intent} class="h-10 object-contain" /><span class="text-xs">33%</span>)
+          B2(<img src=${snakeStrike.intent} class="h-10 object-contain" /><span class="text-xs">33%</span>)
           C2(<img src=${bigBite.intent} class="h-10 object-contain" /><span class="text-xs">33%</span>)
         end
-        1-3Daggers~~~Moves
-        4Daggers~~~Moves
+        subgraph 4Daggers [4 Daggers]
+          B3(<img src=${snakeStrike.intent} class="h-10 object-contain" /><span class="text-xs">67%</span>)
+          C3(<img src=${bigBite.intent} class="h-10 object-contain" /><span class="text-xs">33%</span>)
+        end
       end
       subgraph Dagger [<img src=${base}/assets/act3/elites/dagger.webp class="h-10 object-contain inline" />]
         D(${stab.effect}<img src=${stab.intent} class="h-10 object-contain" />)
         E(${explode.effect}<img src=${explode.intent} class="h-10 object-contain" />)
         D==>E
       end
+  `,
+  moves: `
+    flowchart-elk TB
+      A(${summon.effect}<img src=${summon.intent} class="h-10 object-contain" /><span class="text-xs">Max Consecutive: 2</span>)
+      B(${snakeStrike.effect}<img src=${snakeStrike.intent} class="h-10 object-contain" /><span class="text-xs">Max Consecutive: 1</span>)
+      C(${bigBite.effect}<img src=${bigBite.intent} class="h-10 object-contain" /><span class="text-xs">Max Consecutive: 1</span>)
   `,
   img: `${base}/assets/act3/elites/reptomancer.webp`,
   name: "Reptomancer",
