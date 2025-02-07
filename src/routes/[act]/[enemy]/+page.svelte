4ecb2dd022afd3cea41378719fadba47fb3cd218
Jefferson Hovey <jefferson.hovey@gmail.com>
2025-02-07 01:47:23 -0500

    feat: Awakened One link to Cultist

--- src/routes/[act]/[enemy]/+page.svelte
+++ src/routes/[act]/[enemy]/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+  import { base } from "$app/paths";
   import { page } from "$app/state";
   import Graph from "$lib/components/Graph.svelte";
   import { all } from "$lib/enemies";
-  import { base } from "$app/paths";
 
   const enemy = $derived(all.get(page.params.enemy)!());
 </script>
@@ -13,21 +13,27 @@
 
 {#if enemy.slug === "gremlin-leader"}
   <a href="{base}/act1/gremlins" >
-    <div class="h-24 w-24 rounded border-1 border-secondary p-2">
+    <div class="absolute bottom-0 left-0 h-24 w-24 rounded border-1 border-secondary p-2">
       <img src="{base}/assets/act1/monsters/gremlins.webp" alt="gremlins" class="h-full w-full object-contain" />
     </div>
   </a>
 {:else if enemy.slug === "slime-boss"}
   <div class="flex">
     <a href="{base}/act1/acid-slimes" >
-      <div class="h-24 w-24 rounded border-1 border-secondary p-2">
+      <div class="absolute bottom-0 left-0 h-24 w-24 rounded border-1 border-secondary p-2">
         <img src="{base}/assets/act1/monsters/acid-slimes.webp" alt="Acid Slimes" class="h-full w-full object-contain" />
       </div>
     </a>
     <a href="{base}/act1/spike-slimes" >
-      <div class="h-24 w-24 rounded border-1 border-secondary p-2">
+      <div class="absolute bottom-0 left-0 h-24 w-24 rounded border-1 border-secondary p-2">
         <img src="{base}/assets/act1/monsters/spike-slimes.webp" alt="Spike Slimes" class="h-full w-full object-contain" />
       </div>
     </a>
   </div>
+{:else if enemy.slug === "awakened-one"}
+  <a href="{base}/act1/cultist" >
+    <div class="absolute bottom-0 left-0 h-24 w-24 rounded border-1 border-secondary p-2">
+      <img src="{base}/assets/act1/monsters/cultist.webp" alt="Cultist" class="h-full w-full object-contain" />
+    </div>
+  </a>
 {/if}
